7e577073ce02fc6e887d9c1995ca353f203aaca8
Julien NICOLAS <julien.nicolas@wazari.fr>
2020-07-12 23:45:59 +0200

    test

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hellsdfo!");
+            Console.WriteLine("Hesdfsdfsdfllsdfo!");
         }
     }
 }
