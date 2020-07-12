7a437e53aace3a033fded5686de200bbe17c076d
Julien NICOLAS <julien.nicolas@wazari.fr>
2020-07-12 23:25:18 +0200

    feuture

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -6,8 +6,7 @@ namespace ConsoleApp7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello!");
         }
     }
 }
